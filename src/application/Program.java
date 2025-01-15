321d447ee7aa39dfde44e3fcc5677fa3ac4b8084
robsonosbor <105309876+robsonosbor@users.noreply.github.com>
2025-01-14 22:35:56 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -46,6 +46,10 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n===== TEST 6: seller delete =====");
+		sellerDao.deleteById(11);
+		System.out.println("Delete completed");
 
 	}
 
